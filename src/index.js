a287a6026ca1a8196e44f5c5d41d2c8330c323b4
Manuel Gutierrez <manu.gtzp@gmail.com>
2018-11-13 13:03:54 -0500

    Js folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // @flow
 import './css/index.css';
 import './css/styles.sass';
+import './js/components.js';
 import img from './images/test.jpg';
 import spawn from './images/spawn.jpg';
 
---
+++ src/js/components.js
@@ -0,0 +1,5 @@
+// @flow
+
+var x:string = "Desde otro archivo";
+x = "Reasignando variable desde otro archivo";
+console.log(x);
